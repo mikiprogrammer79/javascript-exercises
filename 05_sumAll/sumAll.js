fcec18427f7c3540cf9a3223a3c263f0c49ce501
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2022-11-21 13:35:52 +0000

    Add code inside the functions to return expected value to pass test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumAll = 0
+    for (let i = num1; i< num2 +1; i++) {
+        sumAll += i
+    }
+    return sumAll
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+   let celsius = ((temp - 32)*(5/9));
+   return celsius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = ((temp * (9/5) + 32));
+  return fahrenheit; 
 };
 
 // Do not edit below this line
